a374b07ace23a4bd2d10d12f2887e42ef7a9ab99
Gearóid Reilly <g00332821@gmit.ie>
2018-11-08 22:42:51 +0000

    Added buttons, labels and text fields.

--- src/ie/gmit/studentmanager/Runner.java
+++ src/ie/gmit/studentmanager/Runner.java
@@ -21,24 +21,74 @@ public class Runner extends Application{
 	//Overrides start function in application
 	@Override
 	public void start(Stage primaryStage) throws Exception{
+		/* Main Scene features */
 		//Create header text
 		Text headerText = new Text("Welcome to the Student Manager \n Select one of the options below");
+		//Create output text
+		TextArea outputText = new TextArea();
 		
 		//Create grid pane to hold items in the scene
 		GridPane mainGridPane = new GridPane();
 		//Set padding on the scene
 		mainGridPane.setPadding(new Insets(10, 10, 10, 10));
 		//Set spacing between objects on the main grid pane
-		mainGridPane.setHgap(10);
-		mainGridPane.setVgap(10);
-		//Set the content to align at the centre
-		mainGridPane.setAlignment(Pos.CENTER);
+		mainGridPane.setHgap(30);
+		mainGridPane.setVgap(30);
+		
+		/* Add student functionality */
+		//Create addButton
+		Button addButton = new Button("Add Student");
+		
+		/* Delete student functionality */
+		//Create deleteButton
+		Button deleteButton = new Button("Delete Student");
+		//Create delete Label
+		Label deleteLabel = new Label("Enter Student Id:");
+		//Create deleteText
+		TextField deleteText = new TextField();
+		
+		/* Search by Id functionality */
+		//Create searchByIdButton
+		Button searchByIdButton = new Button("Search By Student Id");
+		//Create searchById Label
+		Label searchByIdLabel = new Label("Enter Student Id:");
+		//Create searchByIdText
+		TextField searchByIdText = new TextField();
+		
+		/* Search by First Name functionality */
+		//Create searchByNameButton
+		Button searchByNameButton = new Button("Search By First Name");
+		//Create searchByName Label
+		Label searchByNameLabel = new Label("Enter First Name:");
+		//Create searchByNameText
+		TextField searchByNameText = new TextField();
+		
+		/* Show total functionality */
+		//Create showTotalButton
+		Button showTotalButton = new Button("Show total Students");
+		
+		/* Quit functionality */
+		//Create quitButton
+		Button quitButton = new Button("Quit Student Manager");
 		
 		//Adds content to the mainGridPane
 		mainGridPane.add(headerText, 0, 0);
+		mainGridPane.add(addButton, 0, 1);
+		mainGridPane.add(deleteButton, 0, 2);
+		mainGridPane.add(deleteLabel, 1, 2);
+		mainGridPane.add(deleteText, 2, 2);
+		mainGridPane.add(searchByIdButton, 0, 3);
+		mainGridPane.add(searchByIdLabel, 1, 3);
+		mainGridPane.add(searchByIdText, 2, 3);
+		mainGridPane.add(searchByNameButton, 0, 4);
+		mainGridPane.add(searchByNameLabel, 1, 4);
+		mainGridPane.add(searchByNameText, 2, 4);
+		mainGridPane.add(showTotalButton, 0, 5);
+		mainGridPane.add(quitButton, 0, 6);
+		mainGridPane.add(outputText, 0, 7, 3, 1);
 		
 		//Adds values to the mainStage
-		mainScene = new Scene(mainGridPane, 600, 600);
+		mainScene = new Scene(mainGridPane, 1000, 900);
 		
 		//Set the title to primary stage
 		primaryStage.setTitle("Student Manager");
